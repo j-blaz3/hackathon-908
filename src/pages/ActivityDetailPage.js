45688f2a804b6fd1070992d470c1e4ad15e2ce38
j-blalock <justin.m.blalock@gmail.com>
2020-11-24 01:26:59 -0600

    detail page rendering props

--- src/pages/ActivityDetailPage.js
+++ src/pages/ActivityDetailPage.js
@@ -2,8 +2,23 @@ import React from "react";
 
 class ActivityDetailPage extends React.Component {
   state = {};
+
+  componentDidMount() {
+    this.setState({
+      type: "reading",
+      duration: 2,
+      social: "1 player",
+    });
+  }
   render() {
-    return <h1>ActivityDetailPage</h1>;
+    const { type, duration, social } = this.state;
+
+    return (
+      <div className="details">
+        <h1>ActivityDetailPage</h1>
+        <h2>{`${type} - ${duration} - ${social}`}</h2>
+      </div>
+    );
   }
 }
 
